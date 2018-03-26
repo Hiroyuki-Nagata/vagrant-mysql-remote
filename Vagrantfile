52047e1083e74bdabb079c69138ef632cdc6dd96
Hiroyuki Nagata <hiroyuki.nagata@veeva.com>
2018-03-26 11:03:36 +0900

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,33 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "mvbcoding/awslinux"
+  config.vm.box_version = "2017.03.0.20170401"
+  config.vm.network "forwarded_port", guest: 3306, host: 3306
+
+
+  # deploy codes
+  config.vm.provision :shell, privileged: false, inline: <<-SHELL
+    whoami      #=> vagrant
+    echo $HOME  #=> /home/vagrant
+
+    # install packages
+    sudo yum -y update
+    sudo yum -y install mysql
+SHELL
+
+  # Configure your environment variables
+  $set_environment_variables = <<SCRIPT
+tee "/etc/profile.d/myvars.sh" > "/dev/null" <<EOF
+export MYSQL_ROOT_PASSWORD=""
+export MYSQL_DATABASE=test # anything ok
+export MYSQL_USER=youruser
+export MYSQL_PASSWORD=yourpassword
+export MYSQL_ALLOW_EMPTY_PASSWORD=yes
+export IMPORT_SOURCES="your_db_host1,3306,username,password,database1|your_db_host2,3306,username,password,database2"
+EOF
+SCRIPT
+
+  # Set environment variables & import DB
+  #config.vm.provision :file , source: "import-data.sh", destination: "$HOME/import-data.sh"
+  config.vm.provision :shell, inline: $set_environment_variables, run: "always"
+  config.vm.provision :shell, path: "import-data.sh"
+end
---
+++ import-data.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+yum -y install pv
+
+SOURCES=(`echo $IMPORT_SOURCES | tr -s '|' ' '`)
+
+for SOURCE in ${SOURCES[@]}
+do
+    SOURCE_TOKENS=(`echo $SOURCE | tr -s ',' ' '`)
+    TARGET_HOST=${SOURCE_TOKENS[0]}
+    TARGET_PORT=${SOURCE_TOKENS[1]}
+    TARGET_USERNAME=${SOURCE_TOKENS[2]}
+    TARGET_PASSWORD=${SOURCE_TOKENS[3]}
+    TARGET_DATABASE=${SOURCE_TOKENS[4]}
+
+    echo $TARGET_DATABASE
+    GET_SIZE="SELECT table_schema AS \`database\`, ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) AS \`size\` FROM information_schema.TABLES GROUP BY table_schema HAVING \`database\` = '${TARGET_DATABASE}';"
+
+    TARGET_SIZE=$(mysql -s -N -u ${TARGET_USERNAME} -h ${TARGET_HOST} -p${TARGET_PASSWORD} -e "${GET_SIZE}" | awk -F '\t' {'print $2'} )
+    echo "target size for ${TARGET_DATABASE} = ${TARGET_SIZE%.*}M"
+
+    DUMP_FILE=/tmp/${TARGET_HOST}_${TARGET_DATABASE}.sql
+    mysqldump -h $TARGET_HOST -u $TARGET_USERNAME -p$TARGET_PASSWORD $TARGET_DATABASE | pv -s ${TARGET_SIZE%.*}M > $DUMP_FILE
+    if [ $? -ne 0 ]; then
+	echo "Failed to dump $TARGET_HOST" 1>&2
+	exit 1
+    fi
+
+    mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $TARGET_DATABASE;"
+    if [ $? -ne 0 ]; then
+	echo "Failed to create database $TARGET_DATABASE" 1>&2
+	exit 1
+    fi
+
+    mysql -u root --password=$MYSQL_ROOT_PASSWORD $TARGET_DATABASE < $DUMP_FILE
+    if [ $? -ne 0 ]; then
+	echo "Failed to import data $TARGET_DATABASE" 1>&2
+	exit 1
+    fi
+done
